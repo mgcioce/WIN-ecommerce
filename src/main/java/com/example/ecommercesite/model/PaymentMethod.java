a576b3117ae31046b05339008e55ec7b9ecdc9a0
Michael Cioce <mgcioce@gmail.com>
2019-08-02 09:52:02 -0400

    adding authentication and user service stuff

--- src/main/java/com/example/ecommercesite/model/PaymentMethod.java
+++ src/main/java/com/example/ecommercesite/model/PaymentMethod.java
@@ -1,27 +1,27 @@
-package com.example.ecommercesite.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.time.LocalDate;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-public class PaymentMethod {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String cardNumber;
-    private String nameOnCard;
-    private LocalDate expirationDate;
-    private String securityCode;
-    private cardProvider cardProvider;
-}
+//package com.example.ecommercesite.model;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import java.time.LocalDate;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//public class PaymentMethod {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String cardNumber;
+//    private String nameOnCard;
+//    private LocalDate expirationDate;
+//    private String securityCode;
+//    private cardProvider cardProvider;
+//}
--- src/main/java/com/example/ecommercesite/repository/ProductRepository.java
+++ src/main/java/com/example/ecommercesite/repository/ProductRepository.java
@@ -1,14 +1,24 @@
 package com.example.ecommercesite.repository;
 
 import com.example.ecommercesite.model.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends CrudRepository <Product, Integer> {
     List<Product> findByBrandAndCategory(String brand, String category);
     List<Product> findByBrand(String brand);
     List<Product> findByCategory(String category);
 
     Product findById(long id);
+
+    @Query("SELECT DISTINCT p.brand FROM Product p")
+    List<String> findDistinctBrands();
+
+    @Query("SELECT DISTINCT p.category FROM Product p")
+    List<String> findDistinctCategories();
 }
