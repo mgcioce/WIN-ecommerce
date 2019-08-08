27368ef8b6a78aa1f15e88dfe19011dac3af1084
mgcioce <mgcioce@gmail.com>
2019-08-08 08:17:17 -0400

    adding more product pictures to the static resources folder and adding an sql statement for adding those resources in Products.sql

--- Products.sql
+++ Products.sql
@@ -3,3 +3,10 @@ VALUES ('iPhone X', '64GB, iOS 11, space gray', 999.00, 9999, 'Apple', 'Phone', 
        ('iPhone 8', '64GB, iOS 11, silver', 799.00, 9999, 'Apple', 'Phone', 5, 'iphone8-silver-select-2017.jpg'),
        ('C7 OLED', '65" Smart TV', 3000, 9999, 'LG', 'TV', 5, 'C7_ST_Desktop_Front.jpg'),
        ('Macbook Pro', '15 laptop, 512GB SSD', 2800, 15000, 'Apple', 'Computer', 5,  'apple_mlh32ll_a_15_4_macbook_pro_with_1293726.jpg');
+
+INSERT INTO Product (name, description, price, quantity, brand, category, rating, image)
+VALUES ('Ghost Camcorder', '4K 144frames/second ghost camcorder, 2TB onboard storage', 9999.00, 9999, 'Toshiba', 'Ghost Hunting', 5, 'ghost-camcorder.jfif'),
+       ('Ghost Trap', 'Actually Catches Ghosts!', 5999.00, 9999, 'Ghostbusters, Inc', 'Ghost Hunting', 5, 'ghostbusters-trap.jfif'),
+       ('How to Ghost Hunt', '30MB of text and pictures printed on dead trees', 19.99, 9999, 'GhostHunters', 'Books', 5, 'ghosthunting-book.jfif'),
+       ('Thermal Imaging Sensor', 'up to 10000 BTUs', 999.00, 9999, 'Ghost Imaging,LLC', 'Ghost Hunting', 5, 'heat-detector.jfif'),
+       ('Proton Pack', 'An unlicensed nuclear accelerator worn on your back', 8999.00, 9999, 'Ghostbusters, Inc', 'Ghost Hunting', 5, 'protonpack.jfif');
